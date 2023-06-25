10e7957e09aa46e4dd2996ce395b3ed8e26872ad
Sanjana Lakkimsetty <114553649+SanjanaLakkimsetty@users.noreply.github.com>
2023-06-25 20:11:59 +0530

    Add files via upload

---
+++ Calcularor oasis/script.js
@@ -0,0 +1,31 @@
+let expression = '';
+
+function appendNumber(number) {
+  expression += number;
+  document.getElementById('result').value = expression;
+}
+
+function appendOperator(operator) {
+  expression += operator;
+  document.getElementById('result').value = expression;
+}
+
+function calculateResult() {
+  try {
+    const result = eval(expression);
+    document.getElementById('result').value = result;
+    expression = '';
+  } catch (error) {
+    document.getElementById('result').value = 'Error';
+  }
+}
+
+function clearResult() {
+  expression = '';
+  document.getElementById('result').value = '';
+}
+
+function deleteLastEntry() {
+  expression = expression.slice(0, -1);
+  document.getElementById('result').value = expression;
+}
